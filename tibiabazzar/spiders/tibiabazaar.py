355d8443a17822f3e2a46e547fec9e94337762fd
marcoswds <51304108+marcoswds@users.noreply.github.com>
2020-08-30 16:40:58 -0300

    fixing bug

--- tibiabazzar/spiders/tibiabazaar.py
+++ tibiabazzar/spiders/tibiabazaar.py
@@ -5,71 +5,81 @@ class TibiabazaarSpider(scrapy.Spider):
     name = 'tibiabazaar'
     allowed_domains = ['www.tibia.com/charactertrade/?subtopic=pastcharactertrades']
     start_urls = ['http://www.tibia.com/charactertrade/?subtopic=pastcharactertrades/']
+    next_page = 'http://www.tibia.com/charactertrade/?subtopic=pastcharactertrades&currentpage='
 
     def parse(self, response):
 
         pages = response.css(".PageLink a::text")[-1].get()
 
-        for i in range(int(pages)-1):
+        current_page = response.css(".CurrentPageLink::text").get()
 
-            chars = response.css(".Auction")
+        next_page = self.next_page + str(int(current_page) +1)
 
-            for char in chars:
-                name = char.css("a::text").get()
-                server =  char.css("a::text")[1].get()
-                descricao = char.css(".AuctionHeader::text").get()
-                arrDesc = descricao.split("|")
-                lvl = arrDesc[0].replace("Level: ", "").strip()
-                vocation = arrDesc[1].replace("Vocation:", "").strip()
-                sex = arrDesc[2].strip()
+        chars = response.css(".Auction")
 
-                item1 = char.css(".CVIconObject::attr(title)")[0].get().replace('""', "").strip()
-                item2 = char.css(".CVIconObject::attr(title)")[1].get().replace('""', "").strip()
-                item3 = char.css(".CVIconObject::attr(title)")[2].get().replace('""', "").strip()
-                item4 = char.css(".CVIconObject::attr(title)")[3].get().replace('""', "").strip()
+        for char in chars:
+            name = char.css("a::text").get()
+            server =  char.css("a::text")[1].get()
+            descricao = char.css(".AuctionHeader::text").get()
+            arrDesc = descricao.split("|")
+            lvl = arrDesc[0].replace("Level: ", "").strip()
+            vocation = arrDesc[1].replace("Vocation:", "").strip()
+            sex = arrDesc[2].strip()
 
-                start = char.css(".ShortAuctionDataValue::text")[0].get()
-                end = char.css(".ShortAuctionDataValue::text")[1].get()
+            item1 = char.css(".CVIconObject::attr(title)")[0].get().replace('""', "").strip()
+            item2 = char.css(".CVIconObject::attr(title)")[1].get().replace('""', "").strip()
+            item3 = char.css(".CVIconObject::attr(title)")[2].get().replace('""', "").strip()
+            item4 = char.css(".CVIconObject::attr(title)")[3].get().replace('""', "").strip()
 
-                status_auction = char.css(".ShortAuctionDataLabel::text")[2].get()
-                if status_auction == "Minimum Bid:" :
-                    status_auction = "Not Sold"
-                else:
-                    status_auction = "Sold"
+            start = char.css(".ShortAuctionDataValue::text")[0].get()
+            end = char.css(".ShortAuctionDataValue::text")[1].get()
 
-                b_tag = char.css(".ShortAuctionDataValue")[2]
-                tc = b_tag.css("b::text").get().replace(',', "").replace('.', "")
+            status_auction = char.css(".ShortAuctionDataLabel::text")[2].get()
+            if status_auction == "Minimum Bid:" :
+                status_auction = "Not Sold"
+            else:
+                status_auction = "Sold"
 
-                items = TibiabazzarItem()
+            b_tag = char.css(".ShortAuctionDataValue")[2]
+            tc = b_tag.css("b::text").get().replace(',', "").replace('.', "")
 
-                items['name'] = name
-                items['server'] =  server
-                items['lvl'] = lvl
-                items['vocation'] = vocation
-                items['sex'] = sex
-                items['item1'] = item1
-                items['item2'] = item2
-                items['item3'] = item3
-                items['item4'] = item4
-                items['start'] = start
-                items['end'] = end
-                items['status_auction'] = status_auction
-                items['tc'] = tc
+            items = TibiabazzarItem()
 
-                special_features = char.css(".SpecialCharacterFeatures").extract_first()
+            items['name'] = name
+            items['server'] =  server
+            items['lvl'] = lvl
+            items['vocation'] = vocation
+            items['sex'] = sex
+            items['item1'] = item1
+            items['item2'] = item2
+            items['item3'] = item3
+            items['item4'] = item4
+            items['start'] = start
+            items['end'] = end
+            items['status_auction'] = status_auction
+            items['tc'] = tc
 
-                if special_features:
-                    entries = char.css(".Entry")
+            special_features = char.css(".SpecialCharacterFeatures").extract_first()
 
-                    for num, entry in enumerate(entries,start=1):
-                        items['special_features'+str(num)] = entry.css("div ::text").get()
+            if special_features:
+                entries = char.css(".Entry")
 
-                yield items
-          
+                for num, entry in enumerate(entries,start=1):
+                    items['special_features'+str(num)] = entry.css("div ::text").get()
 
-            next_page = response.css(".PageLink a::attr(href)")[i].get()
+            yield items
+
+        print(current_page)
+        print(pages)
+
+
+        if int(current_page) < int(pages):         
+
+            print("---------------------")
+            print(next_page)
 
             yield scrapy.Request(
                 response.urljoin(next_page),
-                callback=self.parse
+                callback=self.parse,
+                dont_filter=True
             )
